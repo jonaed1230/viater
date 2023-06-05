adbeed2d21dbb62e9830a4f58759b84fbaec9a4c
jonaed1230 <jonaed.com.bd@gmail.com>
2023-06-06 01:43:51 +0600

    feat(dashboard): setup app for vercel deployment

--- api/app/index.ts
+++ api/app/index.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import userRouter from './user/router';
 
 const router = Router();
+router.get('/', (req, res) => {
+  res.send('Welcome to viater API route');
+});
 router.use('/user', userRouter);
 
 export default router;
