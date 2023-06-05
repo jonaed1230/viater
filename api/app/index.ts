e839843f4d25af26238ae698c3a61cfc520a1f6a
jonaed1230 <jonaed.com.bd@gmail.com>
2023-06-05 21:06:02 +0600

    fix(api): added / router response

--- api/app/index.ts
+++ api/app/index.ts
@@ -3,6 +3,9 @@ import userRouter from './user/router';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.send('Welcome to viater API');
+});
 router.use('/user', userRouter);
 
 export default router;
