dd0fbe9c1fb965b397441cc1742dcd374a6d6742
jonaed1230 <jonaed.com.bd@gmail.com>
2023-08-22 04:28:21 +0600

    feat(api): added bit controller and route

--- api/app/index.ts
+++ api/app/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import userRouter from './user/router';
 import orderRouter from './order/router';
+import bidRouter from './bid/router';
 
 const router = Router();
 router.get('/', (req, res) => {
@@ -8,5 +9,6 @@ router.get('/', (req, res) => {
 });
 router.use('/user', userRouter);
 router.use('/order', orderRouter);
+router.use('/bid', bidRouter);
 
 export default router;
